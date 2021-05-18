f7aef93911d89432a4dfc1a16acaf7382be4db60
gl91306 <75343148+gl91306@users.noreply.github.com>
2021-05-18 11:29:05 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Script created by oxmc
+#Made for Lunar-Client by gl91306
+
+#install modprobe if not already installed
+if ! command -v modprobe >/dev/null;then
+  
+  if [ -f /usr/bin/apt ];then
+    sudo apt update
+    sudo apt install -y modprobe || echo "Failed to install modprobe."
+  else
+    error "Failed to find any package manager to install modprobe."
+  fi
+fi
+
+#Run modprobe fuse
+sudo modprobe fuse
+
+#Download client
+wget https://github.com/gl91306/lunar/blob/master/lunarclient-2.7.0-armv7l.AppImage?raw=true
+
+#Change perms of Launcher
+sudo chmod +x ~/lunarclient-2.7.0-armv7l.AppImage
+
+#Run launcher
+~/lunarclient-2.7.0-armv7l.AppImage
+
+#Handle error about jvm
+#Copy jdk version into ~/.lunarclient/jre/zulu8.52.0.23-ca-fx-jre8.0.282-linux_x64
+sudo cp /opt/jdk/jdk1.8.0_251/jre ~/.lunarclient/jre/zulu8.52.0.23-ca-fx-jre8.0.282-linux_x64
+
+#Then make menu button
+echo "Creating a desktop entry for Lunar-Client..."
+echo "[Desktop Entry]
+Name=\"Lunar-Client for Rpi\"
+Comment=\"Lunar-Client for Rpi made by PiKATchu on Discord.\"
+Exec=~/lunarclient-2.7.0-armv7l.AppImage
+Icon=~/.lunarclient/icon.png
+Categories=Utility;
+Type=Application
+Terminal=false" > "~/.local/share/applications/Lunar-Client.desktop"
