cd9f26d557e9a4c3445f3b47102eb4eed8a78f89
gl91306 <75343148+gl91306@users.noreply.github.com>
2021-06-24 14:33:44 -0700

    create unsintall scrpit

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+cd
+rm -rf .lunarclient
+rm -rf lunarclient-2.7.3b-armv7l.AppImage
+rm -rf lunarclient-2.7.3a-armv7l.AppImage
+rm -rf lunarclient-2.7.2-armv7l.AppImage
+rm -rf lunarclient-2.7.0-armv7l.AppImage
