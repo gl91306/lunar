20d3110ce00452ded8258ece63653afad437ecd0
gl91306 <75343148+gl91306@users.noreply.github.com>
2021-06-25 00:34:42 -0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,5 @@ rm -rf lunarclient-2.7.3b-armv7l.AppImage
 rm -rf lunarclient-2.7.3a-armv7l.AppImage
 rm -rf lunarclient-2.7.2-armv7l.AppImage
 rm -rf lunarclient-2.7.0-armv7l.AppImage
+rm -rf '$HOME/Desktop/Lunar Client'
+rm -rf "$HOME/.local/share/applications/Lunar-Client.desktop"
