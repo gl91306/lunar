83b16ea831c977d46ce9c5a69c5d3972b772e81b
gl91306 <gl91306@student.musd.org>
2021-07-10 01:18:09 -0700

    lunar 2.7.3c uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -4,5 +4,5 @@ rm -rf lunarclient-2.7.3b-armv7l.AppImage
 rm -rf lunarclient-2.7.3a-armv7l.AppImage
 rm -rf lunarclient-2.7.2-armv7l.AppImage
 rm -rf lunarclient-2.7.0-armv7l.AppImage
-rm -rf '$HOME/Desktop/Lunar Client'
+rm -rf '$HOME/lunarassets'
 rm -rf "$HOME/.local/share/applications/Lunar-Client.desktop"
